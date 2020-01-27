1366584de2e072ab29f8962c6d1ddc30e43d9ca1
“kanjanaloet” <kanjana_loet@kkumail.com>
2020-01-27 16:32:22 +0700

    16:32

---
+++ FizzBuzz.java
@@ -0,0 +1,14 @@
+void tearDown() throws Exception {
+	}
+
+	@Test
+	void test_input_is_1_output_1() {
+		FizzBuzz  FB = new FizzBuzz();
+		int input = FB.printStringinput(1);
+		assertEquals(1, input);
+		
+	}
+
+public int printStringinput(int number) {
+		return number;
+	}

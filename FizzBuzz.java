6a918d66968b022a19d93b7549b85b05c7e44aa7
Thanagon Phiwsuay <thanagonp@kkumail.com>
2020-01-28 07:33:29 +0000

    Updated FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,14 +1,12 @@
-void tearDown() throws Exception {
-	}
 
 	@Test
-	void test_input_is_1_output_1() {
+public static void test_input_is_1_output_1() {
 		FizzBuzz  FB = new FizzBuzz();
 		int input = FB.printStringinput(1);
 		assertEquals(1, input);
 		
 	}
 
-public int printStringinput(int number) {
+public static int printStringinput(int number) {
 		return number;
 	}

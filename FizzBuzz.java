852cac3f75a686c38d00124f0aa1c97b85681382
“kanjanaloet” <kanjana_loet@kkumail.com>
2020-01-29 14:38:56 +0700

    EDIT LINE 19

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -16,7 +16,7 @@ public class FizzBuzz {
 	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter ur number:");
+		System.out.print("Enter your number:");
 		int n = input.nextInt();
 		System.out.println("Resule"+printStringinput(n));
 	}
